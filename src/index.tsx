a17de70c6a1af127ad93d600f3ee6cf3a5eca50d
mizchi <miz404@gmail.com>
2017-07-12 19:25:24 +0900

    Add allowJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import foo from './foo'
 
 const a: number = 3
 console.log(a)
-
-function myFunc(parameter: number) {
-  throw new Error('Not implemented yet')
-}
+foo()
 
 ReactDOM.render(<h1>Hello</h1>, document.querySelector('main'))
