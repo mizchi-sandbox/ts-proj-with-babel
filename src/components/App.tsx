8bec78968bffa7b8e121b083d06f25e7cad1f2ef
mizchi <miz404@gmail.com>
2017-07-12 20:09:19 +0900

    Add props check

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+type Props = {
+  title: string
+}
+export default function App(props: Props) {
+  return (
+    <h1>
+      {props.title}
+    </h1>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import App from './components/App'
 import foo from './foo'
 
 const a: number = 3
 console.log(a)
 foo()
 
-ReactDOM.render(<h1>Hello</h1>, document.querySelector('main'))
+ReactDOM.render(<App title="Welcome" />, document.querySelector('main'))
